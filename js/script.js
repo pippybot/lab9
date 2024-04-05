cf308461f9b3e74e4d5a7dab5a928071add1dffe
pippybot <150129081+pippybot@users.noreply.github.com>
2024-04-04 23:35:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
-function init(){
-document.addEventListener('DOMContentLoaded', function() {
-  // Get references to the input and button elements
-  var input = document.getElementById('entryinput');
+function init() {
   var button = document.getElementById('entrybutton');
-  var output = document.getElementById('textoutput');
 
-  // Add click event listener to the button
-  button.addEventListener('click', function() {
-    // Get the value of the input field
-    var inputValue = input.value;
-
+  function showAlertAndDisplayText() {
+    var textbox = document.getElementById('entryinput');
+    var inputValue = textbox.value;
+    
     // Show an alert with the input value prefixed by your name
     alert("Your Name: " + inputValue);
-
+    
     // Update the text of the h2 element
-    output.textContent = inputValue;
-  });
-});
+    document.getElementById('textoutput').textContent = inputValue;
+  }
+
+  button.addEventListener('click', showAlertAndDisplayText);
+}
+
 window.addEventListener('load', init);
